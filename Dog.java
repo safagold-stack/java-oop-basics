9e0d82fdbcabf148c09b60f9682ceec20a127c2b
safagold-stack <safa.omega.gold@gmail.com>
2026-04-27 13:06:35 +0300

    add Dog Class

---
+++ Dog.java
@@ -0,0 +1,8 @@
+public class Dog {
+    public String name;
+    public double age;
+
+    public void printDetails(){
+        System.out.println("Name is: " + name + " Age is = " + age);
+    }
+}
